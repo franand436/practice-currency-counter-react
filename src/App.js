180cb6de20451df3c589fc9e1129a2865f1b3eb5
Francisco Andrade <88457714+paco-801@users.noreply.github.com>
2022-02-17 18:47:27 -0700

    Currency Conversion API integrated.
    Successfully tied in currency conversion API.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL = 'https://freecurrencyapi.net/api/v2/latest?apikey=0d7dce40-905c-11ec-949f-d550ba5f4fec'
+
 function App() {
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[])
+
   return (
     <>
     <h1>Convert</h1>
